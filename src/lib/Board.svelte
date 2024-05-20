35bb38657a82e2ffa4c075d1a37da094105636f9
gideon <gideon.monterosa@gmail.com>
2024-05-20 18:39:40 +0200

    add a simple component for the board

---
+++ src/lib/Board.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  const tiles = [['', ''], ['', '']];
+</script>
+
+<div class="flex">
+  {#each tiles as row}
+    <div>
+      {#each row as tile}
+        <div class="bg-gray-400 h-8 w-8 m-1">
+          {tile}
+        </div>
+      {/each}
+    </div>
+  {/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+  import Board from '$lib/Board.svelte';
+</script>
+
 <h1 class="text-3xl font-bold underline">
-  Hello world!
+  Minesweeper
 </h1>
+
+<Board/>
